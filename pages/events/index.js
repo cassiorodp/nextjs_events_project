a67257225e538b917700d719be216e6120b52d60
Cássio Pereira <cassiorodp@gmail.com>
2022-09-19 15:46:01 -0300

    feat: implement event search functionallity

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,22 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 import { getAllEvents } from '../../dummy-data';
 
 function EventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
