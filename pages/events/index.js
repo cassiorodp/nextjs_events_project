3b2753ec523f2ceb6bed729d265a191a5a0ef38b
Cássio Pereira <cassiorodp@gmail.com>
2022-10-14 11:17:05 -0300

    feat: add getStaticProps to events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,11 +2,11 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 
-function EventsPage() {
+function EventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -17,9 +17,20 @@ function EventsPage() {
   return (
     <>
       <EventsSearch onSearch={findEventsHandler} />
-      <EventList items={events} />
+      <EventList item={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default EventsPage;
