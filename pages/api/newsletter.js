2369433ab5c81055cdc636ddfe212ab64f8c2011
Cássio Pereira <cassiorodp@gmail.com>
2022-12-14 10:31:43 -0300

    feat: add mongodb connection

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email } = req.body;
 
@@ -7,7 +9,17 @@ function handler(req, res) {
       return;
     }
 
-    console.log(email);
+    const mongoDBPassword = process.env.MONGO_DB_PASSWORD;
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://cassiorodp:${mongoDBPassword}@startercluster.0cnvf.mongodb.net/?retryWrites=true&w=majority`
+    );
+    const db = client.db('newsletter');
+
+    await db.collection('emails').insertOne({ email });
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
