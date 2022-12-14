b9451b0774546bb9b46fc314abd34b47e3700b2b
Cássio Pereira <cassiorodp@gmail.com>
2022-12-14 15:33:58 -0300

    fix: change notification local

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 import Head from 'next/head';
-import Notification from '../components/ui/notification';
 import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
@@ -20,7 +19,6 @@ function MyApp({ Component, pageProps }) {
           />
         </Head>
         <Component {...pageProps} />
-        <Notification />
       </Layout>
     </NotificationContextProvider>
   );
