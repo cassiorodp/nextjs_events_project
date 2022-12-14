64f08276ad15aa03f9c062376578eed1aa59c62b
Cássio Pereira <cassiorodp@gmail.com>
2022-12-14 11:06:42 -0300

    feat: add aux functions

--- helpers/mongo-connection.js
+++ helpers/mongo-connection.js
@@ -10,3 +10,19 @@ async function getMongoClient() {
 }
 
 export default getMongoClient;
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+
+  const result = await db.collection(collection).insertOne(document);
+
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+
+  return documents;
+}
