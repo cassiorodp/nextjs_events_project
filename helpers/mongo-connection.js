42b017d5c53aab69576dbdd8d68206bba95db20e
Cássio Pereira <cassiorodp@gmail.com>
2022-12-14 11:12:51 -0300

    fix: update db name

--- helpers/mongo-connection.js
+++ helpers/mongo-connection.js
@@ -4,7 +4,7 @@ const mongoDBPassword = process.env.MONGO_DB_PASSWORD;
 
 async function getMongoClient() {
   const client = await MongoClient.connect(
-    `mongodb+srv://cassiorodp:${mongoDBPassword}@startercluster.0cnvf.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://cassiorodp:${mongoDBPassword}@startercluster.0cnvf.mongodb.net/events?retryWrites=true&w=majority`
   );
   return client;
 }
